4a2c54917efdb3a5bdae1d610a416cfd25d56d89
highmileage <rcsellers@gmail.com>
2017-02-08 00:19:15 -0600

    Get simple response based on environment variables

--- update_rides.py
+++ update_rides.py
@@ -8,9 +8,9 @@ resp = requests.get(authorization_url)
 
 # grab and authorization code
 code = response.get('code')
-access_token = client.exchange_code_for_token(client_id=16055, client_secret=os.environ['STRAVA_CLIENT_SECRET'], code=code)
+access_token = client.exchange_code_for_token(client_id=16055, client_secret=os.environ['STRAVA_CLIENT_SECRET'], code=os.environ['STRAVA_CODE'])
 client.access_token = access_token
 
 for activity in client.get_activities(after = "2016-01-01T00:00:00Z", limit=1):
     a = activity
-    print("{0.name} {0.moving_time} {0.gear_id} {0.gear.name}".format(activity))
+    print("{0.name} {0.moving_time} {0.gear_id}".format(activity))
