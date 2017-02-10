ca6c436ff907a77f41a6d992bec9d3fd7454cff5
highmileage <rcsellers@gmail.com>
2017-02-09 23:55:39 -0600

    Make changes to write out values

--- update_rides.py
+++ update_rides.py
@@ -3,15 +3,14 @@ import os
 import requests
 
 client = Client()
-# authorization_url = client.authorization_url(client_id=16055, redirect_uri='http://localhost:8282/authorized')
-# resp = requests.get(authorization_url)
+# Run the lines below and then follow the line and grant access
+# Set the response code as the env variable STRAVA_CODE_WRITE
+# authorization_url = client.authorization_url(client_id=16055, redirect_uri='http://localhost:8282/authorized', scope='write')
 
-# grab and authorization code
-# code = response.get('code')
-access_token = client.exchange_code_for_token(client_id=16055, client_secret=os.environ['STRAVA_CLIENT_SECRET'], code=os.environ['STRAVA_CODE'])
+access_token = client.exchange_code_for_token(client_id=16055, client_secret=os.environ['STRAVA_CLIENT_SECRET'], code=os.environ['STRAVA_CODE_WRITE'])
 client.access_token = access_token
 
-for activity in client.get_activities(after = "2016-01-01T00:00:00Z", limit=10):
+for activity in client.get_activities(after = "2016-01-01T00:00:00Z"):
     if activity.name in ['Evening Ride', 'Morning Ride'] and activity.gear_id == 'b249182' and activity.distance.num < 10000:
         print("Updating {0.id} from gear_id {0.gear_id} to 'b352561'").format(activity)
         client.update_activity(activity.id, gear_id='b352561')
